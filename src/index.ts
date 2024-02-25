34062c050c7df572aa771741487f9edab257fae5
Téo Conan <teo@teoconan.fr>
2024-02-25 14:35:44 +0100

    WIP 2

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,142 @@
-enum Result {
+import specialsChars from './ressources/specialschars.json' assert { type: 'json' };
+import reservedWords from './ressources/reserved.json' assert { type: 'json' };
+import lettersMap from './ressources/lettersMap.json' assert { type: 'json' };
+import badWords from './ressources/badWords.json' assert { type: 'json' };
+
+export enum DecentUsernameProblem {
+    Undefined,
     Ok,
-    Warning,
-    Violation,
+    Banned,
+    Reserved,
+    MinLengthViolation,
+    MaxLengthViolation,
 }
 
 export class DecentUsername {
-    private specialsChars: string[] = [];
-    private reservedWords: string[] = [];
-    private customWords: string[] = [];
-    private mappers: any;
+    public readonly minLength: number;
+    public readonly maxLength: number;
 
-    private engine: any = null;
+    private text: string;
+    public variants: string[] = [];
+    public problemType: DecentUsernameProblem;
+    public violationText: string;
 
-    private possibilities: string[];
+    public specialsChars: string[];
+    public reservedWords: string[];
+    public lettersMap: any;
+    public badWords: string[];
 
-    constructor() {
-        var BadWords = require('bad-words');
-        var frenchBadwordsList = require('french-badwords-list');
-        this.specialsChars = require('./res/specialschars.json');
-        this.reservedWords = require('./res/reserved.json');
-        this.mappers = require('./res/mappers.json');
+    constructor(text: string, minLength: number = 4, maxLength: number = 16) {
+        if (!text) throw 'Please provide username';
 
-        this.engine = new BadWords({ placeHolder: '' });
-        this.engine.addWords(...this.reservedWords);
-        this.engine.addWords(...frenchBadwordsList.array);
+        this.minLength = minLength;
+        this.maxLength = maxLength;
+
+        this.text = text.toLocaleLowerCase();
+        this.problemType = DecentUsernameProblem.Ok;
+        this.specialsChars = specialsChars;
+        this.reservedWords = reservedWords;
+        this.lettersMap = lettersMap;
+        this.badWords = badWords;
     }
 
-    validate(text: string): Result {
-        this.possibilities = this.getPosibilities(text);
-        return Result.Ok;
-    }
+    validate(): boolean {
+        if (this.text.length < this.minLength) {
+            this.violationText = this.text;
+            this.problemType = DecentUsernameProblem.MinLengthViolation;
+            return;
+        }
 
-    getPosibilities(text: string): string[] {
-        let output: string[] = [];
-        const keys = Object.keys(this.mappers);
+        if (this.text.length >= this.maxLength) {
+            this.violationText = this.text;
+            this.problemType = DecentUsernameProblem.MaxLengthViolation;
+            return;
+        }
 
-        for (const mapLetter of keys) {
-            const charsToReplace = Reflect.get(this.mappers, mapLetter);
+        this.forChars(this.specialsChars, (i, c) => {
+            this.text = this.text.replaceAll(c, '');
+        });
 
-            for (const char of charsToReplace) {
-                output.push(text.replace(char, mapLetter));
+        this.variants = this.getVariations();
+        this.clearRepeat();
+
+        for (const p of this.variants) {
+            for (const r of this.reservedWords) {
+                if (p == r) {
+                    this.violationText = this.text;
+                    this.problemType = DecentUsernameProblem.Reserved;
+                    return;
+                }
+            }
+
+            let hidder = '';
+            for (const b of this.badWords) {
+                if (this.text.includes(b)) {
+                    hidder = '*'.repeat(b.length);
+                    this.violationText = this.text.replaceAll(b, hidder);
+                    this.problemType = DecentUsernameProblem.Banned;
+                }
             }
         }
+        return true;
+    }
+
+    clearRepeat() {
+        const text = this.text.split('');
+        let output = [];
+
+        for (let i = 0; i < this.text.length; i++) {
+            // Si le charactère courant n'est pas 0 ou max et que les 3 lettres courante ne sont pas égaux, alors
+            if (
+                i != 0 &&
+                i != text.length &&
+                text[i - 1] == text[i] &&
+                text[i] == text[i + 1]
+            ) {
+                continue;
+            }
+
+            output.push(text[i]);
+        }
+
+        this.variants.push(output.join());
+    }
+
+    get(): string {
+        return this.text;
+    }
+
+    isValid(): boolean {
+        return this.problemType == DecentUsernameProblem.Ok;
+    }
+
+    removeSpecialChars(text: string): string {
+        return text;
+    }
+
+    getVariations(): string[] {
+        let output: string[] = [this.text];
+
+        this.forChars(this.lettersMap, (mapLetter, char) => {
+            const changedText = this.text.replaceAll(char, mapLetter);
+            if (changedText != this.text) output.push(changedText);
+        });
 
         return output;
     }
 
-    validateAll(texts: string[]): Result[] {
-        return texts.map((t) => this.validate(t));
+    forChars(
+        input: any,
+        callback: (i: string, v: string, line: string) => void
+    ) {
+        const keys = Object.keys(input);
+
+        for (const key of keys) {
+            const values = Reflect.get(input, key);
+
+            for (const value of values) {
+                callback(key, value, values);
+            }
+        }
     }
 }
--- src/test.ts
+++ src/test.ts
@@ -1,7 +1,76 @@
-import { DecentUsername } from '.';
+import { DecentUsername, DecentUsernameProblem } from './index.js';
+import testUsernames from './ressources/testsUsername.json' assert { type: 'json' };
 
-console.log('Test');
+import logSymbols from 'log-symbols';
+import chalk from 'chalk';
 
-const du = new DecentUsername();
+const tests = [
+    '"tropppe`="',
+    '"asser`="',
+    '"assssswer`="',
+    '"access$^ù`="',
+    '"access$^`="',
+    'teo"',
+    'tropp"',
+    '"',
+    'asspicture',
+    'pornpony',
+    'hitler3302',
+    'hitleer3302',
+    'hitl3r3302',
+    'settings',
+    'account',
+    'butterorgasm',
+    //...testUsernames,
+];
 
-du.validate('access');
+var testsPassedCounter = 0;
+
+console.log('');
+console.log('');
+console.log(chalk.bold('\t\tTests'));
+console.log('');
+console.log('');
+
+const start = new Date().getTime();
+
+for (const username of tests) {
+    const du = new DecentUsername(username);
+    du.validate();
+
+    if (!du.isValid()) {
+        console.log(
+            logSymbols.warning,
+            chalk.yellow(
+                username +
+                    ' is not valid (' +
+                    DecentUsernameProblem[du.problemType] +
+                    ')'
+            )
+        );
+
+        continue;
+    }
+
+    testsPassedCounter++;
+}
+
+let elapsed = (new Date().getTime() - start) / 1000;
+
+console.log(
+    logSymbols.success,
+    chalk.bold('Tests passed : ' + testsPassedCounter + '/' + tests.length)
+);
+
+const timePerHit = elapsed / tests.length;
+let color = chalk.green;
+let icon = logSymbols.success;
+
+if (timePerHit > 0.0005) {
+    color = chalk.red;
+    icon = logSymbols.warning;
+}
+
+console.log('');
+console.log(icon, color('Time spent : ' + elapsed + 'ms'));
+console.log(icon, color('Time spent per hit : ' + timePerHit));
